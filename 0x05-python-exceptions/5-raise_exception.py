5baf791422bc972a300827efcd3a99dd5c8c4e24
leumars-o <samex150@gmail.com>
2024-01-25 16:01:38 +0100

    task-5, 7, 8, 9, 10 added

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A function that raises a type exception.
+
+Return:
+    - None
+Description:
+    - No module should be imported
+"""
+
+
+def raise_exception():
+    raise TypeError
