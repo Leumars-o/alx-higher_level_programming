a419529aa932c9eee7a1193e49ee3ab1c51c0bb1
leumars-o <samex150@gmail.com>
2024-01-25 15:43:01 +0100

    task-4 modified to hande type and value error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -21,9 +21,9 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = [0] * list_length
     for i in range(list_length):
         try:
-            division = int(my_list_1[i]) / int(my_list_2[i])
+            division = (my_list_1[i]) / (my_list_2[i])
             new_list[i] = division
-        except ValueError:
+        except (TypeError, ValueError):
             print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
