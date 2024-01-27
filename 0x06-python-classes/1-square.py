0fd6e863772132d0ef2a7508e2ef82b0452029f1
leumars-o <samex150@gmail.com>
2024-01-27 17:55:15 +0100

    task-1: sqaure with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A module that creates a Square classs
+
+Attributes:
+    - Square: a class Square that defines squares
+
+"""
+
+
+class Square:
+    """A class that defines a square
+
+    Attributes:
+        __size: private instance attribute to Square
+    """
+    def __init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/test.py
@@ -0,0 +1,5 @@
+a = [1, "hello", 3]
+b = [4, 5, 6]
+for b in a:
+    print(b)
+    
