faf9a839a6d9bd0cca6ebc0bd5d3fcfa51eca602
leumars-o <samex150@gmail.com>
2024-01-27 17:15:41 +0100

    initial commit: README, task-0: my first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""An empty class
+
+Parameters:
+    - None
+Return:
+    - None
+Description:
+    - An empty class Square that defines as square.
+    - No modules should be imported.
+"""
+
+
+class Square:
+    pass
