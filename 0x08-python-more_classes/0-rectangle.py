8ef9ff4c615dc478a906292009b307e7e1f6b87b
leumars-o <samex150@gmail.com>
2024-02-02 15:40:05 +0100

    initial commit: task-0: simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Rectangle definition and usage
+
+Classes:
+    - Rectangle: a class that defines rectangles
+"""
+
+
+class Rectangle:
+    """A Class that defines rectangles
+
+    Methods:
+        None
+    """
