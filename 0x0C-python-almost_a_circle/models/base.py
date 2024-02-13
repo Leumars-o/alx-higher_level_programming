a28ff35a358a319b8cc483e56e9a631775dd3e3f
leumars-o <samex150@gmail.com>
2024-02-13 05:28:35 +0100

    task-0 modified and documented

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
+"""A module that defines a class `Base`
+
+Classes:
+    Base: base class that manages 'id' attributes
+"""
+
+
 class Base:
+    """A class that defines and manages 'id' attributes
+
+    Methods:
+        - __init__: intialization method
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialization method
+
+        Args:
+            id (_type_, int): _description_. Defaults to None.
+        """
         if id is not None:
             self.id = id
         else:
