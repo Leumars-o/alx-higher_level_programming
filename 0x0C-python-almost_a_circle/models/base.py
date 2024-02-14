84c4192008eb6ee1dc190e849a02b361e74cf9a4
leumars-o <samex150@gmail.com>
2024-02-14 07:43:49 +0100

    task-18: dictionary to instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -65,3 +65,13 @@ class Base:
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new_instance = cls(1, 1)
+            else:
+                new_instance = (1)
+            new_instance.update(**dictionary)
+            return new_instance
