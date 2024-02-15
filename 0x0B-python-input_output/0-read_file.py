1ad02a7545667c4d5bcd1b8c294bcb9e3499c970
leumars-o <samex150@gmail.com>
2024-02-15 18:09:13 +0100

    task-0: Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pthon3
+    """A function that reads from a text file
+    """
+def read_file(filename=""):
+    """Function reads a texts file and prints to stdout
+
+    Args:
+        filename (str, optional): path to file to read.
+    """
+    with open(filename, encoding='utf8') as f:
+        print(f.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,7 @@
+0x0B. Python - Input/Output
+Python
+
+*Python Scripts*
+Edited with VS Code
+All your files were interpreted/compiled on Ubuntu 20.04 LTS using python3 (version 3.8.5)
+This code should uses the pycodestyle (version 2.8.*)
