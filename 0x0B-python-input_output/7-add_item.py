2256cde17c753b373b1adb789e76c042d4bb0b37
leumars-o <samex150@gmail.com>
2024-02-16 18:41:07 +0100

    task-7: load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A module that provides a function that appends args to a json file
+"""
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+def append_args():
+    """A function that adds all arguments to a python list and saves
+    them to a file
+
+    Returns:
+        list: list of all arguments
+    """
+    filename = 'add_item.json'
+    arg_list = sys.argv[1:]
+    try:
+        json_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        json_list = []
+    json_list += arg_list
+    save_to_json_file(json_list, filename)
+    return json_list
