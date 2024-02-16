21f942f4cb1626e3b58046b2ad1b35fcbd3788b7
leumars-o <samex150@gmail.com>
2024-02-16 18:51:54 +0100

    task-7: script modified

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,17 +2,12 @@
 """A module that provides a function that appends args to a json file
 """
 import sys
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-def append_args():
-    """A function that adds all arguments to a python list and saves
-    them to a file
+if __name__ == "__main__":
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-    Returns:
-        list: list of all arguments
-    """
     filename = 'add_item.json'
     arg_list = sys.argv[1:]
     try:
@@ -22,4 +17,3 @@ def append_args():
     for arg in arg_list:
         json_list.append(arg)
     save_to_json_file(json_list, filename)
-    return json_list
