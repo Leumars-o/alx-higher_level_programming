c529ededfb7c6d5269f354a5c60cc7307022eb32
leumars-o <samex150@gmail.com>
2024-02-17 15:32:24 +0100

    task-8: class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A module that provides a function that returns a dictionary
+description
+"""
+
+
+def class_to_json(obj):
+    """A function that returns the dictionary desc with
+    simple data structure for json serialization of an object
+
+    Args:
+        obj (ant):  (list, dictionary, string, integer and boolean)
+
+    Returns:
+        dict
+    """
+    return __dict__
