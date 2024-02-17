40f5f625bfc733aa05f8f992f0fcaf55a66bb202
leumars-o <samex150@gmail.com>
2024-02-17 16:20:59 +0100

    task-10: student to json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A module that provides a student defining class
+"""
+
+
+class Student:
+    """A class `Student` that defines and creates students
+    """
+    def __init__(self, first_name, last_name, age):
+        """A method that initializes the Student class
+
+        Args:
+            first_name (str): students first name
+            last_name (str): students last name
+            age (int): students age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """A method that returns a dict representation of
+        a Student instance
+
+        Returns:
+            dict
+        """
+        if isinstance(attrs, list):
+            for item in attrs:
+                if isinstance(item, str):
+                    return {
+                        k: getattr(self, k) for k in attrs if hasattr(self, k)
+                        }
+        else:
+            return self.__dict__
