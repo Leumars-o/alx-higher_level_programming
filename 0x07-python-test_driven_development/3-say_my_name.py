383951d85b46db4f4a65f6d24afd5b673fdef49a
leumars-o <samex150@gmail.com>
2024-02-19 07:41:23 +0100

    task-2: say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""A module that provides a function that says your name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """A function that says your name
+
+    Args:
+        first_name (str): your first name
+        last_name (str, optional): Your last name. Defaults to "".
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
