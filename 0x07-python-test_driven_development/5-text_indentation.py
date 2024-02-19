d939f840deccbfed1fabe05d60344ce4a7cce6b7
leumars-o <samex150@gmail.com>
2024-02-19 11:01:22 +0100

    task-4: text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,16 @@
 #!/usr/bin/pyhton3
+"""A module that provides a funtion that prints a formatted
+text
+"""
+
+
 def text_indentation(text):
+    """A function that prints a text with 2 new lines
+    after each of these characters: '.', '?' and ':'
+
+    Args:
+        text (str): string to format
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     special_chars = ('.', '?', ':')
