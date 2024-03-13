f2d7f54d99ecdb98ac8becd668fd7d865b829f0d
leumars-o <samex150@gmail.com>
2024-03-13 18:24:22 +0100

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,10 @@
 -- Script that creates another table in the database and adds multiple rows
 CREATE TABLE
 second_table (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT,
     name VARCHAR(256),
     score INT
 );
+
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
