bda3157303eb3e3d6e9e5eb28d631418e0cfd873
leumars-o <samex150@gmail.com>
2024-03-13 18:04:59 +0100

    full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- Script that printts the full description
 -- of the table from the database
-SELECT column_name, data_type, character_maximum_length
-FROM information_schema.columns
-WHERE table_name = 'first_table';
+SHOW CREATE TABLE first_table;
