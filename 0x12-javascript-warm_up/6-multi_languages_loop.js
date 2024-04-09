b015c67b453072f47c1675bac2adfb029b170ebe
leumars-o <samex150@gmail.com>
2024-04-09 05:38:17 +0100

    loop to languages code modified

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
+
 const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const string of myArray) {
-  console.log(myArray[string]);
+let i;
+
+for (i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
 }
