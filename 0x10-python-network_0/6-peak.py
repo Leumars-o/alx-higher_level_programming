19eba75cd66942ebad18eaaf5e48c986af2f3612
leumars-o <samex150@gmail.com>
2024-04-26 16:50:20 +0100

    find a peak function commented

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-"""function that finds a "Peak" in a list of unsorted integers"""
+"""This module contains a 'find_peak' function."""
 
 
 def find_peak(list_of_integers):
+    """function that finds a "Peak" in a list of unsorted integers
+
+    Args:
+        list_of_integers (list): a list of unsorted integers
+
+    Returns:
+        int: the peak value, or None if list is empty
+    """
+
     if not list_of_integers:
         return None
     numbers = sorted(list_of_integers)
