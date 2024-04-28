bf2f7c65830a5be2f92e43541a4b1ab3839053de
leumars-o <samex150@gmail.com>
2024-04-28 11:08:12 +0100

    Github API interview quizz

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,12 +7,11 @@ import sys
 if __name__ == "__main__":
     rep_name = sys.argv[1]
     user_name = sys.argv[2]
-    url = "https://api.github.com/repos/"
-    + user_name + "/" + rep_name + "/commits"
+    url = f"https://api.github.com/repos/{user_name}/{rep_name}/commits"
     response = requests.get(url)
-    json = response.json()
+    commits = response.json()
 
-    for commit in json[::-1][:10]:
+    for commit in commits[:10]:
         sha = commit["sha"]
         author = commit["commit"]["author"]["name"]
         print(f"{sha}: {author}")
